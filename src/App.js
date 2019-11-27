6f77de096ed4c785128fcf9b556937f3071734b5
Luciana Menezes <menezessluciana@gmail.com>
2019-11-27 10:15:47 -0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import StudentEvalution from './pages/StudentEvaluation';
 import StudentProgress from './pages/StudentProgress';
+import moment from 'moment';
 import './App.css';
 
+moment.locale('pt-BR');
 
 function App() {
   return (
-    <StudentProgress/>
+    <StudentEvalution/>
   );
 }
 
