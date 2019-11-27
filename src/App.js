d9121535223d59f9251fe4b51533bb28a6297fab
Luciana Menezes <menezessluciana@gmail.com>
2019-11-27 10:59:25 -0200

    App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import StudentEvalution from './pages/StudentEvaluation';
 import StudentProgress from './pages/StudentProgress';
-import CompanyForm from './components/Students/List'
+import CompanyForm from './components/Students/List';
+import BookingTrainning from './pages/BookingTrainning';
 import Layout from './components/Layout';
 
 function App() {
   return (
     <Layout>
-      <StudentProgress/>
+      <BookingTrainning/>
     </Layout>
   );
 }
